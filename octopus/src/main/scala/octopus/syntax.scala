d2441afd69aeb912b1f217192dea29c5b3c64585
Piotr Krzemiński <pio.krzeminski@gmail.com>
2017-07-13 18:18:55 +0200

    normalization dsl syntax

--- octopus/src/main/scala/octopus/syntax.scala
+++ octopus/src/main/scala/octopus/syntax.scala
@@ -4,16 +4,24 @@ import scala.concurrent.{ExecutionContext, Future}
 
 object syntax {
 
-  implicit class ValidationOps[T](val obj: T) extends AnyVal {
+  implicit class OctopusOps[T](val obj: T) extends AnyVal {
 
     def validate(implicit v: Validator[T]): ValidationResult[T] =
       new ValidationResult(obj, v.validate(obj))
 
     def isValid(implicit v: Validator[T]): Boolean =
       validate.isValid
+
+    def normalize(implicit n: Normalizer[T]): T =
+      n.normalize(obj)
+
+    def normalizeAndValidate(implicit n: Normalizer[T], v: Validator[T]): ValidationResult[T] = {
+      val normalizedObj = n.normalize(obj)
+      new ValidationResult(normalizedObj, v.validate(normalizedObj))
+    }
   }
 
-  implicit class AsyncValidationOps[T](val obj: T) extends AnyVal {
+  implicit class AsyncOctopusOps[T](val obj: T) extends AnyVal {
 
     def validateAsync(implicit av: AsyncValidator[T],
                       ec: ExecutionContext): Future[ValidationResult[T]] =
