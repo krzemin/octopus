3dd1ef298a7b97736450ff213125004d4aa935ef
Robert <viniolli@gmail.com>
2018-06-04 00:26:35 +0200

    Adjust App[Future] implementation to scala 2.11.11

--- octopus/src/main/scala/octopus/App.scala
+++ octopus/src/main/scala/octopus/App.scala
@@ -21,7 +21,8 @@ trait LowPriorityAppImplicits {
     override def pure[A](a: A): Future[A] = Future.successful(a)
 
     override def map2[A, B, C](first: Future[A], second: Future[B])(combine: (A, B) => C): Future[C] =
-      first.zipWith(second)(combine)(ec)
+      // Reimplement to Future.zipWith after dropping scala 2.11.11 support
+      first.zip(second).map(combine.tupled)
 
     override def recover[A, B <: A](app: Future[A], f: Throwable => B): Future[A] = {
       app.recover {
