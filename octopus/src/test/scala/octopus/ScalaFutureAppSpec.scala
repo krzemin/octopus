06697bcf4072dce22ecab795b6aab82981992621
Robert <viniolli@gmail.com>
2018-07-07 15:41:39 +0200

    Add test for App[Future]

---
+++ octopus/src/test/scala/octopus/ScalaFutureAppSpec.scala
@@ -0,0 +1,22 @@
+package octopus
+
+import octopus.example.domain.BaseAsyncMonadSpec
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.{AsyncWordSpec, MustMatchers}
+
+import scala.concurrent.Future
+
+class ScalaFutureAppSpec
+  extends AsyncWordSpec
+    with MustMatchers
+    with BaseAsyncMonadSpec[Future]
+    with ScalaFutures
+    with IntegrationPatience {
+
+  override def extractValueFrom[A](mval: Future[A]): Future[A] = mval
+
+  "Base Scala future app implementation" should {
+    behave like validateSimpleEmail(App[Future])
+  }
+
+}
