621aac877dede12bcd8d434b45d1603cbabdf527
rwalerow <robert.walerowicz@softwaremill.com>
2019-04-06 14:51:07 +0200

    Rename ApplicativeError param from am to M

--- octopusCats/src/main/scala/octopus/async/cats/implicits.scala
+++ octopusCats/src/main/scala/octopus/async/cats/implicits.scala
@@ -7,21 +7,21 @@ import scala.language.higherKinds
 
 object implicits {
 
-  implicit def catsAppError[M[_]](implicit ae: ApplicativeError[M, Throwable]): AppError[M] = new AppError[M] {
+  implicit def catsAppError[M[_]](implicit M: ApplicativeError[M, Throwable]): AppError[M] = new AppError[M] {
 
     def pure[A](a: A): M[A] =
-      ae.pure(a)
+      M.pure(a)
 
     def failed[A](why: Throwable): M[A] =
-      ae.raiseError(why)
+      M.raiseError(why)
 
     def map[A, B](ma: M[A])(f: A => B): M[B] =
-      ae.map(ma)(f)
+      M.map(ma)(f)
 
     def map2[A, B, C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
-      ae.map2(ma, mb)(f)
+      M.map2(ma, mb)(f)
 
     def recover[A, B <: A](ma: M[A], f: PartialFunction[Throwable, B]): M[A] =
-      ae.recover(ma)(f)
+      M.recover(ma)(f)
   }
 }
